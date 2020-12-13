3f9105b32042347c9c1273a37d2257df404b2e15
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2020-12-13 00:29:45 -0300

    Change `resample` parameter to match PIL options.

--- utils/scripts/convert-images.py
+++ utils/scripts/convert-images.py
@@ -44,7 +44,7 @@ parser = argparse.ArgumentParser(
 parser.add_argument('input_files', nargs='+', help='Input images. Accepts Unix styled wildcards.')
 parser.add_argument('--platform', action='append', choices=PLATFORM_NAMES, default=PLATFORM_NAMES,
     help='Platform to generate images for. Can be informed more than once. If ommited, generates for all plaforms.')
-parser.add_argument('--resample', choices=['nearest', 'box', 'bilinear', 'hamming', 'bicubic', 'lanczos'], default='nearest',
+parser.add_argument('--resample', choices=['nearest', 'bilinear', 'bicubic', 'antialias'], default='nearest',
     help='Resampling filter to use when resizing image. Default is "nearest".')
 args = parser.parse_args()
 
