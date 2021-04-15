2afe7f2a4fb8ba22446462d752961aa1d7829701
Anthony Beaucamp <ant_the_mohican@hotmail.com>
2021-04-15 17:03:12 +0900

    Tech Demo: simplified charmap code.

--- projects/demos/src/demo-charmap.c
+++ projects/demos/src/demo-charmap.c
@@ -5,30 +5,18 @@ extern const char keyNext;
 
 extern unsigned char worldMaxX, worldMaxY;	// See: units/graphics/charmap.c
 
-#if defined __APPLE2__
-	unsigned char charColors[] = {};    //  All colors are pre-assigned in the char sheet
-#elif defined __ATARI__
-	unsigned char charColors[] = { 0x00, 0x0c, 0x78, 0x62, 0x12 };   // Black, White, Light Blue, Dark Blue, Red
-#elif defined __CBM__
-	unsigned char charColors[] = { BLACK, WHITE, LBLUE };
-#elif defined __LYNX__
-	unsigned char charColors[] = {};    //  All colors are pre-assigned in the char sheet
-#elif defined __ORIC__
-	unsigned char charColors[] = {};    //  All colors are pre-assigned in the char sheet
-#endif
-
 int DemoCharmap(void) 
 {	
 	unsigned char joy, scroll = 0, x = (32-CMP_COLS/2), y = (16-CMP_ROWS/2);
 	
 	// Prepare charmap
-	InitCharmap(0, CMP_COLS, 0, CMP_ROWS-1);
+	InitCharmap(0, CMP_COLS, 0, CMP_ROWS-1);	// Display field (x1,x2,y1,y2)
+	LoadCharset("quedex.chr");					// Character set (contains 128 chars)
+	LoadTileset("level1.tls", 40, 2, 2);		// Tile number (40) and size (2x2)
+	LoadCharmap("level1.map", 64, 32);			// Size of map in tiles (64x32)
 	ClearCharmap();
-	LoadCharset("quedex.chr", charColors);
-	LoadTileset("level1.tls", 40, 2, 2);
-	LoadCharmap("level1.map", 64, 32);
-	ShowCharmap();
 	DrawCharmap(x,y);
+	ShowCharmap();
 	
 	// Print instructions
 	inkColor = WHITE; paperColor = BLACK;
