604f8d34011a9fe1a8fa4e8b3408b66aa14075a4
Anthony Beaucamp <ant_the_mohican@hotmail.com>
2022-01-05 23:42:55 +0900

    Grubs Demo: Updated to latest API spec and finished to implement paths.

---
+++ projects/grubs/src/definitions.h
@@ -0,0 +1,67 @@
+
+#define MAX_GRUBS 4
+#define MAX_PROJS 1
+
+#define STATE_PHYS 1
+#define STATE_JUMP 2
+#define STATE_MOVE 4
+#define STATE_WEAP 8
+#define STATE_LOAD 16
+
+#define PROJ_NULL  0
+#define PROJ_LOAD  1
+#define PROJ_MOVE  2
+
+#define KEYFRAME_MLEFT  0
+#define KEYFRAME_JLEFT  3
+#define KEYFRAME_MRIGHT 4
+#define KEYFRAME_JRIGHT 7
+#define KEYFRAME_DEAD   15
+#define KEYFRAME_WEAP   8
+#define KEYFRAME_PROJ   14
+
+#define NUMFRAME_MOVE   3
+#define NUMFRAME_JUMP   1
+#define NUMFRAME_WEAP   3
+
+#define EXPLODE YELLOW
+#define HEALTH_LOW RED
+#define HEALTH_MED ORANGE
+#if defined __APPLE2__
+	#define SKY 		LBLUE
+	#define HEALTH_HIGH LGREEN
+	#define GRND_OFFST	7
+#elif defined __ATARI__
+	#define SKY 		BLUE
+	#define HEALTH_HIGH GREEN
+	#define GRND_OFFST	4
+#elif defined __CBM__
+	#define SKY 		CYAN
+	#define HEALTH_HIGH GREEN
+	#define GRND_OFFST	4
+#elif defined __ORIC__
+	#define SKY 		BLACK
+	#define HEALTH_HIGH MGREEN
+	#define GRND_OFFST	5
+#elif defined __LYNX__
+	#define SKY 		LBLUE
+	#define HEALTH_HIGH GREEN
+	#define GRND_OFFST	7
+#endif
+
+// Grub structure
+typedef struct {
+	unsigned int x, y;
+	signed char zAcc;
+	unsigned int wAng;
+	unsigned char health, index, state;
+	unsigned char keyFrame, motionFrame, numFrames;
+	unsigned char weapFrame;
+} Grub;
+
+// Projectile structure
+typedef struct {
+	signed int x, y;
+	signed char xVel, yVel;
+	unsigned char index, owner, state;
+} Proj;
--- projects/grubs/src/grubs.c
+++ projects/grubs/src/grubs.c
@@ -1,73 +1,7 @@
 
 #include <cc65.h>
 #include "unity.h"
-
-#define MAX_GRUBS 4
-#define MAX_PROJS 1
-
-#define STATE_PHYS 1
-#define STATE_JUMP 2
-#define STATE_MOVE 4
-#define STATE_WEAP 8
-#define STATE_LOAD 16
-
-#define PROJ_NULL  0
-#define PROJ_LOAD  1
-#define PROJ_MOVE  2
-
-#define KEYFRAME_MLEFT  0
-#define KEYFRAME_JLEFT  3
-#define KEYFRAME_MRIGHT 4
-#define KEYFRAME_JRIGHT 7
-#define KEYFRAME_DEAD   15
-#define KEYFRAME_WEAP   8
-#define KEYFRAME_PROJ   14
-
-#define NUMFRAME_MOVE   3
-#define NUMFRAME_JUMP   1
-#define NUMFRAME_WEAP   3
-
-#define EXPLODE YELLOW
-#define HEALTH_LOW RED
-#define HEALTH_MED ORANGE
-#if defined __APPLE2__
-	#define SKY 		LBLUE
-	#define HEALTH_HIGH LGREEN
-	#define GRND_OFFST	7
-#elif defined __ATARI__
-	#define SKY 		BLUE
-	#define HEALTH_HIGH GREEN
-	#define GRND_OFFST	4
-#elif defined __CBM__
-	#define SKY 		CYAN
-	#define HEALTH_HIGH GREEN
-	#define GRND_OFFST	4
-#elif defined __ORIC__
-	#define SKY 		BLACK
-	#define HEALTH_HIGH MGREEN
-	#define GRND_OFFST	5
-#elif defined __LYNX__
-	#define SKY 		LBLUE
-	#define HEALTH_HIGH GREEN
-	#define GRND_OFFST	7
-#endif
-
-// Grub structure
-typedef struct {
-	unsigned int x, y;
-	signed char zAcc;
-	unsigned int wAng;
-	unsigned char health, index, state;
-	unsigned char keyFrame, motionFrame, numFrames;
-	unsigned char weapFrame;
-} Grub;
-
-// Projectile structure
-typedef struct {
-	signed int x, y;
-	signed char xVel, yVel;
-	unsigned char index, owner, state;
-} Proj;
+#include "definitions.h"
 
 clock_t gameClock, projClock;
 unsigned char curGrub = 0;
@@ -79,26 +13,15 @@ unsigned int xStart[4] = { 60, 120, 180, 240 };
 unsigned char *names[4] = { "SAM", "JOE", "TOM", "LUC" };
 
 // Sprite definitions
-#define spriteFrames 16
 #if defined __APPLE2__
-	#define spriteCols   7
-	#define spriteRows   13
 	unsigned char spriteColors[] = { };	 // Colors are pre-assigned in the sprite sheet
 #elif defined __ATARI__
-	#define spriteCols   8
-	#define spriteRows   13
 	unsigned char spriteColors[] = { 0x2a, 0x10, 0x2a, 0x10, 0x2a, 0x10, 0x2a, 0x10, 0x0e, 0x14, 0x00, 0x00};
 #elif defined __ORIC__
-	#define spriteCols   12
-	#define spriteRows   13
 	unsigned char spriteColors[] = { SPR_AIC, SPR_AIC, SPR_AIC, SPR_AIC, SPR_AIC, SPR_AIC, SPR_AIC, SPR_AIC };  // AIC color for faster drawing!
 #elif defined __CBM__
-	#define spriteCols   12
-	#define spriteRows   21
 	unsigned char spriteColors[] = { 0, 0, 0, 0, PINK, PINK, PINK, PINK, RED, WHITE};
 #elif defined __LYNX__
-	#define spriteCols   7
-	#define spriteRows   11
 	unsigned char spriteColors[] = { 0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef,   // Default palette
 									 0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef,   // Default palette
 									 0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef,   // Default palette
@@ -598,7 +521,7 @@ int main(void)
 	
 	// Setup sprites
 	LoadSprites("sprites.dat");
-	SetupSprites(spriteFrames, spriteCols, spriteRows, spriteColors);	
+	SetupSprites(spriteColors);	
 	
 	// Show splash screen
 	SplashScreen();
