bcb724fa49bdf20e93694db09c7771de48473194
Anthony Beaucamp <ant_the_mohican@hotmail.com>
2022-01-21 16:03:22 +0900

    Easynet demo is now fully working!

--- projects/easynet/src/easynet.c
+++ projects/easynet/src/easynet.c
@@ -5,21 +5,22 @@
 #define LEN_NAME  8
 #define FPS       10
 
+
 typedef struct {
 	unsigned char x, y, c;
 } player_t;
 
-player_t *player;
+unsigned char myName[LEN_NAME];
 player_t players[NUM_SLOTS];
-unsigned char mySlot, myName[LEN_NAME];
+player_t *player;
 
 int main (void)
 {
-	unsigned char joy, res;
-	unsigned char slot, len;
-	unsigned char input[12];
+	unsigned char joy, res, slot, len, i;
+	unsigned char *packetData, packetLen;
+	unsigned char buffer[40];
 	unsigned int ID, PASS;
-	clock_t timer = clock();
+	clock_t clTimer, svTimer;
 	
 	// Set text mode colors
     textcolor(COLOR_WHITE);
@@ -27,9 +28,11 @@ int main (void)
     bgcolor(COLOR_BLACK);
 	clrscr(); 
 
-#if defined(__LYNX__) || defined(__NES__)
 	// Set virtual keyboard location
+#if defined(__LYNX__)
 	SetKeyboardOverlay(50,70);
+#elif defined(__NES__)
+	SetKeyboardOverlay(10,20);
 #endif
 
 	// Init Network
@@ -40,7 +43,7 @@ int main (void)
 
 	// Host or Join?
 	gotoxy(3,0); cprintf("Easynet Demo (v0.5)");
-	gotoxy(0,1); cprintf("Choose name: "); scanf("%s", myName);  
+	gotoxy(0,1); cprintf("Choose name: "); gets(myName);
 	gotoxy(0,2); cprintf("(H)ost or (J)oin Session?");
 	while (1) {
 	#if defined(__LYNX__) || defined(__NES__)	
@@ -59,9 +62,6 @@ int main (void)
 		// Host new Session (ID:PASS generated automatically)
 		res = EasyHost(EASY_UDP, NUM_SLOTS, &ID, &PASS); // You can use EASY_TCP or EASY_UDP
 		switch (res) {
-		case EASY_OK:	
-			mySlot = easyBuffer[0];
-			break;
 		case EASY_TIMEOUT:
 			gotoxy(0,3); cprintf("Error: connection timed-out");			
 			while (!kbhit()); return EXIT_SUCCESS;		
@@ -70,15 +70,14 @@ int main (void)
 		
 	case KB_J:
 		// Request credential (ID:PASS must match host)
-		gotoxy(0,3); cprintf("Session ID: "); scanf("%i", &ID);  
-		gotoxy(0,4); cprintf("Session PASS: "); scanf("%i", &PASS); 
+		gotoxy(0,3); cprintf("Session ID: "); 
+		gets(buffer); sscanf(buffer, "%i", &ID);  
+		gotoxy(0,4); cprintf("Session PASS: "); 
+		gets(buffer); sscanf(buffer, "%i", &PASS);  
 		
 		// Try joining session
 		res = EasyJoin(EASY_UDP, &ID, &PASS);
 		switch (res) {
-		case EASY_OK:
-			mySlot = easyBuffer[0];
-			break;
 		case EASY_CRED:
 			gotoxy(0,5); cprintf("Error: ID/PASS incorrect");
 			while (!kbhit()); return EXIT_SUCCESS;
@@ -98,65 +97,87 @@ int main (void)
 	
 	//////////////////////////////
 	// Show connection status
-	clrscr(); gotoxy(0,0);
-	cprintf("Session ID:%u / PASS:%u / SLOT:%i/%i", ID, PASS, mySlot, NUM_SLOTS-1);
+	sprintf(buffer, "Session ID:%u / PASS:%u / SLOT:%i/%i", ID, PASS, easySlot, NUM_SLOTS-1);
+	clrscr(); gotoxy(0,0); cprintf(buffer);
 	
 	//////////////////////////////
 	// Setup player data
-	player = &players[mySlot];
-	player->x = clock()%24+8;	// Pseudo-random position
+	player = &players[easySlot];  // easySlot contains our own slot number
+	player->x = clock()%24+8;	  // Pseudo-random position
 	player->y = clock()%15+2;
 	player->c = myName[0];
 	
 	//////////////////////////////
 	// Run mini interactive game
+	clTimer = clock();
+	svTimer = clock();
 	while (!kbhit() || cgetc() != KB_Q) {
-		// Use timer to regulate game
-		if (clock() < timer)
-			continue;
-		timer = clock() + TCK_PER_SEC / FPS;
+		// Use timer to regulate player input
+		if (clock() > clTimer + TCK_PER_SEC/FPS) {
+			// Update timer
+			clTimer = clock();
 
-		// Eraze previous location
-		player = &players[mySlot];
-		gotoxy(player->x, player->y);
-		cprintf(" ");
-		
-		// Move using joystick
-		joy = GetJoy(0);
-		if (!(joy & JOY_UP))    player->y -= 1;
-		if (!(joy & JOY_DOWN))  player->y += 1;
-		if (!(joy & JOY_LEFT))  player->x -= 1;
-		if (!(joy & JOY_RIGHT)) player->x += 1;
-
-		// Constrain coordinates
-		player->x = MAX(player->x, 8);
-		player->x = MIN(player->x, 31);
-		player->y = MAX(player->y, 2);
-		player->y = MIN(player->y, 16);
-		
-		// Show new location
-		gotoxy(player->x, player->y);
-		cprintf("%c", player->c);		
-		
-		// Exchange data over Easynet
-		EasySend(player, 3);		      // Send my player state (3 bytes)
-		while (EasyRecv(0) == EASY_OK) {  // Receive other player states (time-out set to 0)
-			// Get player
-			slot = easyBuffer[0];
-			len  = easyBuffer[1];
-			player = &players[slot];
-			
 			// Eraze previous location
-			if (player->y) {
-				gotoxy(player->x, player->y);
-				cprintf(" ");
-			}
+			player = &players[easySlot];
+			gotoxy(player->x, player->y);
+			putchar(' ');
+			
+			// Move using joystick
+			joy = GetJoy(0);
+			if (!(joy & JOY_UP))    player->y -= 1;
+			if (!(joy & JOY_DOWN))  player->y += 1;
+			if (!(joy & JOY_LEFT))  player->x -= 1;
+			if (!(joy & JOY_RIGHT)) player->x += 1;
+
+			// Constrain coordinates
+			player->x = MAX(player->x, 8);
+			player->x = MIN(player->x, 31);
+			player->y = MAX(player->y, 2);
+			player->y = MIN(player->y, 15);
 			
 			// Show new location
-			memcpy(player, &easyBuffer[2], 3);
 			gotoxy(player->x, player->y);
-			cprintf("%c", player->c);			
-		}		
+			putchar(player->c);		
+				
+			// Broadcast new player state (3 bytes)
+			EasySend(player, 3);
+		}
+		
+		// Receive data from Easynet server (using time-out of 0)
+		packetData = EasyRecv(0, &packetLen);
+		if (packetLen) {
+			// Update timer
+			svTimer = clock();
+			
+			// Decode packet data: (slot,len,data),(slot,len,data),...
+			i = 0;
+			while (i<packetLen) {
+				// Read data chunk
+				slot = packetData[i++];
+				len  = packetData[i++];
+				if (slot != easySlot) {	// Note: our own data is also broadcasted back to us
+					// Clear previous state
+					player = &players[slot];
+					if (player->y) {
+						gotoxy(player->x, player->y);
+						putchar(' ');
+					}
+					
+					// Update player state
+					memcpy(player, &packetData[i], len);
+					gotoxy(player->x, player->y);
+					putchar(player->c);						
+				}
+				i += len;			
+			}		
+		}
+		
+		// Watch-out for time issues....
+		if (clock() > svTimer+10*TCK_PER_SEC) {
+			gotoxy(0,TXT_ROWS-1);
+			cprintf("Connection Timed-out...");
+			break;
+		}
 		
 		// Refresh screen (if needed)
 	#if defined(__LYNX__) || defined(__NES__)
@@ -167,8 +188,7 @@ int main (void)
 	// Quit Easynet graciously
 	EasyQuit();
 	
-	// Clear screen and show left message
-	clrscr(); gotoxy(0,0); cprintf("Left session");			
+	// Wait gracefully
 	cgetc(); while (!kbhit());
 	
 	// Done
